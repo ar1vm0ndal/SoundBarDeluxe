1760a7c48472812837234697ef52d77d5888e58e
Ariv Mondal <arivmondal@utexas.edu>
2025-06-03 18:39:30 -0500

    Purpose: testing simple bit banging Result: No color output but addressable is up to date and sendRET() works well

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,45 @@
 #include <Arduino.h>
+#define outpin 2
+
+inline void delayCycles(uint32_t cycles) {
+  while (cycles--) {
+    __asm__ __volatile__("nop");
+  }
+}
+
+void IRAM_ATTR sendZero() {
+  GPIO.out_w1ts = (1 << outpin);     
+  delayCycles(60);                   
+  GPIO.out_w1tc = (1 << outpin);     
+  delayCycles(240);                   
+}
+
+void IRAM_ATTR sendOne() {
+  GPIO.out_w1ts = (1 << outpin);     
+  delayCycles(144);                  
+  GPIO.out_w1tc = (1 << outpin);     
+  delayCycles(156);                   
+}
+
+void IRAM_ATTR sendRET() {
+  GPIO.out_w1tc = (1 << outpin);
+  delayCycles(14500);                
+}
+
+void IRAM_ATTR sendBits(uint32_t color) {
+  for (int8_t i = 23; i >= 0; i--) {  // MSB first
+    if (color & (1 << i)) sendOne();
+    else sendZero();
+  }
+}
 
 void setup() {
-
+  pinMode(outpin, OUTPUT);
+  digitalWrite(outpin, LOW);
+  Serial.begin(9600);
 }
 
 void loop() {
-  // Toggle message every second
-  Serial.println("Running...");
-  delay(1000);
+  sendBits(0x00FF00);  // Green (in GRB order)
+  sendRET();
 }
