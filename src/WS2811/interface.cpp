5f08cbc05dfc024d857227fd1997065872109c6b
Ariv Mondal <arivmondal@utexas.edu>
2025-06-13 21:55:44 -0500

    function declaration in RAM fixed latency issues

--- src/WS2811/interface.cpp
+++ src/WS2811/interface.cpp
@@ -3,17 +3,17 @@
 
 #define outpin 2
 
-constexpr uint32_t transmissionTime = 97; //full bit transmission cycle of 1.25 us * 80MHz
-constexpr uint32_t zero = 25; //0.5 us/12.5ns = 20 cycles of high for 0 bit
-constexpr uint32_t one = 53; //0.6 us/12.5ns = 48 cycles of high for 1 bit
+constexpr uint32_t transmissionTime = 100; //full bit transmission cycle of 1.25 us * 80MHz
+constexpr uint32_t zero = 20; //0.5 us/12.5ns = 20 cycles of high for 0 bit
+constexpr uint32_t one = 48; //0.6 us/12.5ns = 48 cycles of high for 1 bit
 
-static inline uint32_t cycleCount(){
+static IRAM_ATTR inline uint32_t cycleCount(){
   uint32_t ccount;
   asm volatile ("rsr %0, ccount" : "=a"(ccount)); // Compiler directive to execute ASM and retrieve cycle count from ccount register
   return ccount;
 }
 
-static inline void sendBit(bool b){
+static IRAM_ATTR inline void sendBit(bool b){
 
   const uint32_t highTime = b? one : zero;
   uint32_t startTime = cycleCount();
